5b8cdb96049d5c2ccc7de2a08a613a1506c4d742
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-04-21 00:06:23 +0900

    수정

--- tsproject/src/compoents/button/Button.tsx
+++ tsproject/src/compoents/button/Button.tsx
@@ -1,20 +1,33 @@
-const Button = ({ variant, size, children, ...rest }) => {
-  return (
-    <button
-      style={{
-        backgroundColor: variant === "primary" ? "blue" : "red",
-        fontSize: size === "small" ? "10px" : "20px",
-        color: "white",
-        padding: "10px",
-        borderRadius: "5px",
-        border: "none",
-        cursor: "pointer",
-      }}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant: "primary";
+    size: "small";
+    children: ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    variant,
+    size,
+    children,
+    ...rest
+}) => {
+    return (
+        <button
+            style={{
+                backgroundColor: variant === "primary" ? "blue" : "red",
+                fontSize: size === "small" ? "10px" : "20px",
+                color: "white",
+                padding: "10px",
+                borderRadius: "5px",
+                border: "none",
+                cursor: "pointer",
+            }}
+            {...rest}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
--- week7/src/reactType/reactFC.tsx
+++ week7/src/reactType/reactFC.tsx
@@ -1,12 +1,7 @@
 /**
- *  'ReactNode'는 React 요소 트리에서 노드를 나타낼 수 있는 타입이다.
- *  컴포넌트가 렌더링할 수 있는 모든 것을 포함한다
- *  다음과 같은 것들을 포함할 수 있다.
- *  1.'ReactElement'-JSX 또는 'React.createElement'를 통해 생성된 리액트 엘리먼트
- *  2.'string'및'number'-DOM에서 텍스트 노드로 렌더링 된다
- *  3.'null'alc'boolean'-렌더링할 때 아무것도 표시하지 않습니다.
- *  4.'ReactFragment'-그룹화된 자식을 나타내는 데 사용할 수 있습니다.
- *  5.'ReactPortal'-다른 DOM 하위 트리에 자식을 렌더링할 수 있다.
+ *  React의 함수형 컴포넌트를 정의할 때 사용하는 TypeScript 타입이다
+ *  함수형 컴포넌트에 대한 명시적인 타입 선언을 제공하며
+ *  TypeScript는 컴포넌트의 prop타입과 반환 타입을 체크할 수 있다
  */
 
 import { S } from "../styles/cssStyle";
